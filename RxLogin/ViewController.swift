5273856e4350c91d980355c98e213cc7668839cb
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2022-01-29 14:56:48 +0900

    Initial Commit

---
+++ RxLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxLogin
+//
+//  Created by 三浦　一真 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxLoginUITests/RxLoginUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RxLoginUITestsLaunchTests.swift
+//  RxLoginUITests
+//
+//  Created by 三浦　一真 on 2022/01/29.
+//
+
+import XCTest
+
+class RxLoginUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
