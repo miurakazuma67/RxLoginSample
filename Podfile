133d1210098fa2a42a53440364fd888502325461
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2022-01-30 23:47:34 +0900

    実装

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxLogin' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxLogin
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+  target 'RxLoginTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RxLoginUITests' do
+    # Pods for testing
+  end
+
+end
--- RxLogin/ViewController.swift
+++ RxLogin/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var password1TextField: UITextField!
+    @IBOutlet private weak var password2TextField: UITextField!
+    @IBOutlet private weak var registerButton: UIButton!
+    private var disoseBag: DisposeBag!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let isValidEmail = emailTextField.rx.text
+            .map { $0 ?? "" }
+            .map { $0.count >= 10 }
+        
+        let isValidPassword1 = password1TextField.rx.text
+            .map { $0 ?? "" }
+            .map { $0.count >= 8 }
+        
+        let isValidPassword2 = password2TextField.rx.text
+            .map { $0 ?? "" }
+            .map { $0.count >= 8 }
+        
+        let isValidPassword = Observable.combineLatest(password1TextField.rx.text.map { $0 ?? "" },
+                                                       password2TextField.rx.text.map { $0 ?? "" }
+        )
+            .map { password1, password2 in
+                password1 == password2 && password1.count >= 8
+            }
+            .subscribe( onNext: { [weak self] in
+                self.registerButton.isEnabled = $0
+            } )
+            .disposed(by: disoseBag)
     }
-
-
+    
+    
 }
 
